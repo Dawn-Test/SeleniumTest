eb64e78ead3a9ae3eec0f24469d94d5743bdbcb2
王金龙 <62553705+wangjinlong-ceshi@users.noreply.github.com>
2021-09-08 12:52:56 +0800

    Delete MetLife directory

--- MetLife/common/ui_exception.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-# coding=utf-8
-from selenium.common.exceptions import WebDriverException
-
-
-class LocatorTypeNotSupport(WebDriverException):
-    pass
--- MetLife/page_locators/base_page_locator.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-# coding=utf-8
-
-class BasePageLocator(object):
-    """
-    存放一些公共的定位
-    """
-    pass
-f
